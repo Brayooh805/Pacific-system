fcce7d669011299ea0b731e0a4dd363caa9c554e
Brian Mburu <bmburu@pacific-group.local>
2025-12-01 10:10:47 +0300

    Fix database path for deployment

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# This creates the pacific.db file in the same folder
-SQLALCHEMY_DATABASE_URL = "sqlite:///./pacific.db"
+import os
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+# Get the folder where this file (database.py) is located
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Point to the pacific.db file inside that folder
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'pacific.db')}"
